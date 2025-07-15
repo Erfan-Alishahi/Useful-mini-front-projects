022622a0b4b5b2685edb6dff1271c33efde8228f
Erfan Alishahi <erfanalishahi02@gmail.com>
2025-07-16 02:26:53 +0330

    add login-form and back buttons

--- sign-up project/script.js
+++ sign-up project/script.js
@@ -1,7 +1,30 @@
 const container = document.querySelector('.container');
-const SignUpBtn = document.querySelector('.green-bg button');
+const signUpBtn = document.querySelector('.signup');
+const loginBtn = document.querySelector('.login');
+const backBtn_L = document.querySelector('.back-l');
+const backBtn_R = document.querySelector('.back-r');
 
-SignUpBtn.addEventListener('click',
-() => {
-    container.classList.toggle('change');
+
+let condition = null;
+
+
+signUpBtn.addEventListener('click', () => {
+  container.classList.remove('move-right', 'move-center');
+  container.classList.add('move-left');
+});
+
+loginBtn.addEventListener('click', () => {
+  container.classList.remove('move-left', 'move-center');
+  container.classList.add('move-right');
+  
+});
+
+backBtn_L.addEventListener('click', () => {
+  container.classList.remove('move-right', 'move-left');
+  container.classList.add('move-center');
+});
+
+backBtn_R.addEventListener('click', () => {
+  container.classList.remove('move-right', 'move-left');
+  container.classList.add('move-center');
 });
